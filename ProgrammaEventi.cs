052435cc56f5f1f68d75b9f0627ad8c0e89940ea
Jac22 <jacopo.prudentino@gmail.com>
2023-04-17 11:13:52 +0200

    Creata la classe ProgrammaEventi coi relativi metodi

---
+++ ProgrammaEventi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class ProgrammaEventi
+    {
+        public string Titolo { get; set; }
+        public List<Evento> Eventi { get; set; }
+
+        public ProgrammaEventi(string titolo)
+        {
+            Titolo = titolo;
+            Eventi = new List<Evento>();
+        }
+
+        public void AggiungiEvento(Evento ev)
+        {
+            Eventi.Add(ev);
+        }
+
+        public List<Evento> FiltraData(DateTime data)
+        {
+            return (List<Evento>) Eventi.Where(x => x.Data == data);
+        }
+
+        public static string StringaLista(List<Evento> lista)
+        {
+            string stringaLista = "";
+            foreach (Evento ev in lista)
+            {
+                stringaLista += ev.ToString();
+            }
+            return stringaLista;
+        }
+
+        public int NumeroEventi()
+        {
+            return Eventi.Count;
+        }
+
+        public void SvoutaLista()
+        {
+            Eventi.Clear();
+        }
+
+        public override string ToString()
+        {
+            string stringaLista = Titolo + "\n";
+            foreach (Evento ev in Eventi)
+            {
+                stringaLista += ev.ToString();
+            }
+            return stringaLista;
+        }
+    }
+}
